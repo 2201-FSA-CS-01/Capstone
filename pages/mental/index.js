7a4adcfc8d11c15712bb9782064548007f312464
Peter Cummings <petecummings@hotmail.com>
2022-03-23 14:58:12 -0700

    updated: css with tailwind shadow feature and buttons on physical / mental pages

--- pages/mental/index.js
+++ pages/mental/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import prisma from "../../utils/prisma";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
-import Navbar from "../../components/Navbar.js"
-import Header from "../../components/Header"
-
+import Navbar from "../../components/Navbar";
+import Header from "../../components/Header";
 
 const index = (props) => {
   const { data: session } = useSession();
@@ -12,13 +11,30 @@ const index = (props) => {
     return (
       <div>
         <Header />
-        Mental Goals Page
-        {props.goals
-          ? props.goals.map((g) => {
-              return <div>{g.name}</div>;
-            })
-          : "Loading goals"}
-          <Navbar />
+        <div className="box-border border-2 h-50 w-50 p-4 rounded-md border-solid border-white">
+          <h1 className="heading text-3xl text-slate-100 font-Manrope text-center">
+            Mental Goals
+          </h1>
+          <div>
+            <ul className="flex flex-col items-center ">
+              {props.goals
+                ? props.goals.map((g) => {
+                    return (
+                      <li
+                        //onClick={submitMental}
+                        className="flex flex-col w-5/6 h-16 my-4 text-3xl text-shadow-lg truncate shadow-md rounded-lg bg-gradient-to-r from-green-400 to-blue-500 text-slate-100 font-Manrope shadow-yellow-500/100"
+                      >
+                        <button className="pt-2 justify-items-center ">
+                          {g.name}
+                        </button>
+                      </li>
+                    );
+                  })
+                : "Loading goals"}
+            </ul>
+          </div>
+        </div>
+        <Navbar />
       </div>
     );
   }
@@ -26,7 +42,7 @@ const index = (props) => {
     <>
       Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button>
-      <Link href="/mental" className="">
+      <Link href="/emotional" className="">
         Mental Goals
       </Link>
     </>
