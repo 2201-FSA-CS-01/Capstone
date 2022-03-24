59d26783b5f0c8926079e40d2b58a99e7abb9b14
Peter Cummings <petecummings@hotmail.com>
2022-03-24 09:31:23 -0700

    added: /dashboard link to tomo icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,18 @@
-import tomo from '../public/images/tomo.png';
-import goal from '../public/images/goal.png';
-import user from '../public/images/user.png';
-import Image from 'next/image';
+import tomo from "../public/images/tomo.png";
+import goal from "../public/images/goal.png";
+import user from "../public/images/user.png";
+import Image from "next/image";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="flex flex-col">
       <ul className="flex justify-around w-full p-5 text-center bg-gray-800 border-2 border-white rounded-lg text-stone-800">
-        <li>
-          <Image src={tomo} alt="" height={40} width={40} />
-        </li>
+        <Link href="/dashboard">
+          <li>
+            <Image src={tomo} alt="" height={40} width={40} />
+          </li>
+        </Link>
         <li>
           <Image src={goal} alt="" height={40} width={40} />
         </li>
