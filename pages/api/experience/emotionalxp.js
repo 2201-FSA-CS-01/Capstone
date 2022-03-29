060c4ebe9a634afac0d1cf29189e0e388166c972
Adam Sailsman <adamjaysailsman@gmail.com>
2022-03-29 17:03:10 -0400

    Store user values in DB, and persists

--- pages/api/experience/emotionalxp.js
+++ pages/api/experience/emotionalxp.js
@@ -1,9 +1,9 @@
-import { getSession } from "next-auth/react";
-import prisma from "../../../utils/prisma";
+import { getSession } from 'next-auth/react';
+import prisma from '../../../utils/prisma';
 export default async function handler(req, res) {
   const session = await getSession({ req });
   try {
-    if (req.method === "GET") {
+    if (req.method === 'GET') {
       const user = await prisma.user.findUnique({
         where: {
           email: session.user.email,
@@ -14,5 +14,28 @@ export default async function handler(req, res) {
     }
   } catch (err) {
     console.log(err);
+  }
+  if (req.method === 'PUT') {
+    try {
+      if (session.user.email) {
+        const { emotionalxp } = await prisma.user.findUnique({
+          where: { email: session.user.email },
+        });
+        if (emotionalxp < 100) {
+          const updateEmotionalXp = await prisma.user.update({
+            where: { email: session.user.email },
+            data: {
+              emotionalxp: {
+                increment: 20,
+              },
+            },
+          });
+          res.status(200).send(updateEmotionalXp);
+        }
+        res.end();
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
--- pages/physical/index.js
+++ pages/physical/index.js
@@ -1,15 +1,20 @@
-import React from "react";
-import prisma from "../../utils/prisma";
-import { useSession, signIn, signOut } from "next-auth/react";
-import Link from "next/link";
-import MentalMeter from "../../components/Meters/MentalMeter";
-import PhysicalMeter from "../../components/Meters/PhysicalMeter";
-import EmotionalMeter from "../../components/Meters/EmotionalMeter";
-import Navbar from "../../components/Navbar";
-import { useAppContext } from "../../components/context/state";
-import LoggedOut from '../../pages/loggedOut/index'
+import React from 'react';
+import prisma from '../../utils/prisma';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Link from 'next/link';
+import MentalMeter from '../../components/Meters/MentalMeter';
+import PhysicalMeter from '../../components/Meters/PhysicalMeter';
+import EmotionalMeter from '../../components/Meters/EmotionalMeter';
+import Navbar from '../../components/Navbar';
+import { useAppContext } from '../../components/context/state';
+import LoggedOut from '../../pages/loggedOut/index';
 
 const index = (props) => {
+  const updateXP = async (id) => {
+    await fetch('api/experience/physicalxp', {
+      method: 'PUT',
+    });
+  };
   const myContext = useAppContext();
 
   const { data: session } = useSession();
@@ -18,37 +23,40 @@ const index = (props) => {
     //In return, have <SingleGoals/> comp render here as <SingleGoals/>, then on a separate component, map through props(goals) as below?
     return (
       <div className="py-36">
-        <div className="border-2 border-solid border-white rounded-md mx-4 fixed inset-x-0 top-0 h-50 w-50 p-1 bg-gray-800">
+        <div className="fixed inset-x-0 top-0 p-1 mx-4 bg-gray-800 border-2 border-white border-solid rounded-md h-50 w-50">
           <MentalMeter />
           <PhysicalMeter />
           <EmotionalMeter />
           <br />
         </div>
-        <div className="box-border m-4 h-50 w-50 p-1">
-          <h1 className="heading text-3xl text-slate-100 font-Manrope text-center">
+        <div className="box-border p-1 m-4 h-50 w-50">
+          <h1 className="text-3xl text-center heading text-slate-100 font-Manrope">
             Physical Goals
           </h1>
           <div>
             <ul className="flex flex-col items-center ">
               {props.goals
-                ? props.goals.map((g) => {
-                  return (
-                    <li
-                      key={g.id}
-                      onClick={myContext.submitPhysical}
-                      className="flex flex-col w-5/6 h-16 my-4 text-3xl truncate shadow-md rounded-lg bg-gradient-to-r from-yellow-400 via-gold-500 to-red-500 text-slate-100 font-Manrope shadow-violet-500/100"
-                    >
-                      <button className="pt-4 justify-items-center ">
-                        {g.name}
-                      </button>
-                    </li>
-                  );
-                })
-                : "Loading goals"}
+                ? props.goals.map((goals) => {
+                    return (
+                      <li
+                        key={goals.id}
+                        onClick={() => {
+                          updateXP();
+                          myContext.submitPhysical();
+                        }}
+                        className="flex flex-col w-5/6 h-16 my-4 text-3xl truncate rounded-lg shadow-md bg-gradient-to-r from-yellow-400 via-gold-500 to-red-500 text-slate-100 font-Manrope shadow-violet-500/100"
+                      >
+                        <button className="pt-4 justify-items-center ">
+                          {goals.name}
+                        </button>
+                      </li>
+                    );
+                  })
+                : 'Loading goals'}
             </ul>
           </div>
         </div>
-        <div className="mx-4 fixed inset-x-0 bottom-0">
+        <div className="fixed inset-x-0 bottom-0 mx-4">
           <Navbar />
         </div>
       </div>
@@ -72,7 +80,7 @@ export const getServerSideProps = async () => {
   try {
     const goals = await prisma.task.findMany({
       where: {
-        catagory_name: "physical",
+        catagory_name: 'physical',
       },
     });
 
@@ -83,7 +91,7 @@ export const getServerSideProps = async () => {
     return {
       redirect: {
         permanent: false,
-        destination: "/",
+        destination: '/',
       },
     };
   }
