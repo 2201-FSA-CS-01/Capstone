6ab8d6dcbd4ad4bb2c7466d901db8c95a1f6164b
Peter Cummings <petecummings@hotmail.com>
2022-03-31 13:06:28 -0700

    edit: profile page pwa layout

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import Navbar from '../../components/Navbar';
-import Link from 'next/link';
-import Image from 'next/image';
-import TomoHome from '../../public/images/tomoTreeHouse.gif';
-import Countdown from 'react-countdown';
-import { useAppContext } from '../../components/context/state';
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Navbar from "../../components/Navbar";
+import Link from "next/link";
+import Image from "next/image";
+import TomoHome from "../../public/images/tomoTreeHouse.gif";
+import Countdown from "react-countdown";
+import { useAppContext } from "../../components/context/state";
 
 const Profile = () => {
   const myContext = useAppContext();
   return (
-    <div>
-      <h1 className="box-border p-4 text-3xl text-center border-2 border-white border-solid rounded-md heading text-slate-100 font-Manrope h-50 w-50">
-        User Profile
-      </h1>
-
-      <ul className="flex flex-col text-center slate-800">
-        <li className="text-slate-100 font-Manrope">Name:</li>
-        <li className="text-slate-100 font-Manrope">Email:</li>
-        <li className="text-slate-100 font-Manrope">Experience Pts:</li>
-        <li className="text-slate-100 font-Manrope">Level:</li>
-        <li className="text-slate-100 font-Manrope">Goals Completed:</li>
-      </ul>
-      <Image src={TomoHome} alt="" height={400} className="rounded-md" />
-
-      <div className="box-border p-4 mt-8 text-3xl text-center border-2 border-white border-solid rounded-md text-slate-100 font-Manrope h-50 w-50">
+    <div className="lg: mt-2 lg:grid lg:grid-cols-5 lg:gap-6 ">
+      <div className="box-border p-4 text-3xl text-center border-2 border-white border-solid rounded-md heading text-slate-100 font-Manrope lg:row-start-1 lg:row-end-1 lg:col-start-3 col-end-4">
+        <h1>User Profile</h1>
+      </div>
+      <div className="border-white border-solid rounded-md heading text-slate-100 font-Manrope lg:row-start-2 lg:row-end-2 lg:col-start-3 col-end-4">
+        <ul className="flex flex-col text-center text-slate-100">
+          <br />
+          <li>User Email:</li>
+          <li>User Experience:</li>
+          <br />
+        </ul>
+      </div>
+      <div className="lg:row-start-3 lg:row-end-3 lg:col-start-3 col-end-4">
+        <Image src={TomoHome} alt="" height={400} className="rounded-md" />
+      </div>
+      <div className="box-border p-4 mt-8 text-3xl text-center border-2 border-white border-solid rounded-md text-slate-100 font-Manrope w-96 lg:row-start-4 lg:row-end-4 lg:col-start-3 col-end-4">
         <Countdown date={myContext.countdown} onComplete={myContext.refresh} />
       </div>
-
-      <div className="fixed bottom-0 flex flex-col w-full">
+      <div className="lg:row-start-5 lg:row-end-5 lg:col-start-3 col-end-4">
         <Link href="/loggedOut">
-          <button className="text-center bg-gray-200 border-2 border-white rounded-md shadow-md shadow-black">
+          <button className="text-center mb-2 w-96 bg-gray-200 border-2 border-white rounded-md shadow-md shadow-black my-2">
             Sign out
           </button>
         </Link>
-
+      </div>
+      <div className="inset-x-0 bottom-0 w-100 mt-1 lg:row-start-6 lg:row-end-6 lg:col-start-3 col-end-4">
         <Navbar />
       </div>
     </div>
